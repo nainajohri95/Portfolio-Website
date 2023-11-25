bc66086678c91038c9b4c80e7c0322ca24b2f349
Naina Johri <jhorinaina8@gmail.com>
2023-11-25 21:03:52 +0530

    Intro component created

--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -1,12 +1,50 @@
 import React from "react";
 import "./Intro.css";
-import img from "../../assets/naina.png";
+import bg from "../../assets/NainaImg.png";
+import linkedInImg from "../../assets/linkedin-icon-png-16.jpg";
+import GithubImg from "../../assets/github-icon.jpg";
+import { FaLinkedin } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa6";
 
 const Intro = () => {
+  const openLinkedInProfile = () => {
+    window.open("https://www.linkedin.com/in/naina-johri/", "_blank");
+  };
+  const openGitHubProfile = () => {
+    window.open("https://github.com/nainajohri95", "_blank");
+  };
   return (
     <section id="intro">
-      <div className="introContent"></div>
-      <img src={img} alt="" className="img" />
+      <div className="introContent">
+        <span className="hello">Hello,</span>
+        <span className="introText">
+          I'm <span className="introName">Naina Johri</span> <br /> Front End
+          Developer
+        </span>
+        <p className="introPara">
+          I'm on a mission to craft exceptional web experiences that captivate
+          and connect. With expertise as a Front-End Developer, I breathe life
+          into digital landscapes, blending creativity and interactivity.
+        </p>
+
+        <div className="logo-container">
+          <a
+            href="https://www.linkedin.com/in/naina-johri/"
+            target="_blank"
+            className="logo-link"
+          >
+            <img src={linkedInImg} alt="LinkedIn" className="logo" />
+          </a>
+          <a
+            href="https://github.com/nainajohri95"
+            target="_blank"
+            className="logo-link"
+          >
+            <img src={GithubImg} alt="GitHub" className="logo" />
+          </a>
+        </div>
+      </div>
+      <img src={bg} alt="" className="img" />
     </section>
   );
 };
